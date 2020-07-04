e58b946e738ed8d719dc640cd2a500f8b2288b2d
Yassin Youssoufm <yassinrsc@gmail.com>
2020-07-04 12:52:00 +0300

    set home as the root page

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::prefix('admin')->group(function (){
     Route::view('/','admin.statics.index')->name('statics');
 
 });
+
 // Route::get("{pages?}", 'HomeController@pageTemplate')->name('loadPage');
 
 // Only logged in users can reach this routes
