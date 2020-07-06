15006604dbc2e87ae603fbc59ad7adcd4e2e631a
Yassin Youssoufm <yassinrsc@gmail.com>
2020-07-06 14:40:49 +0300

    update layout

--- resources/views/site/account/index.blade.php
+++ resources/views/site/account/index.blade.php
@@ -5,14 +5,26 @@
     <div class="container">
         <div class="my-shadow py-4">
         <div class="dif">
-            <h4 class="text-center font-weight-bold">مرحبا بك يا {{ auth()->user()->name }}</h4>
+            <h4 class="text-center font-weight-bold">مرحبا بك يا <span class="c-bol">{{ explode(' ', auth()->user()->name)[0] }}</span></h4>
             <span class="d-block text-center"> <img src="./images/flower.svg" alt=""></span>
         </div>
 
         <div class="my-4 kufi">
             <div class=" control-actions">
             <div class=" dde  row ">
-                <div class="col-6">
+                <div class="col-6 col-md-4">
+                    <a href="" class="position-relative saad border pt-3 pb-1 px-1  ">
+                        <div class="">
+                            <div class="text-center">
+                                <i class="fas fa-user icon-f"></i>
+                            </div>
+                            <div>
+                                <h5 class="text-center  manage  c-bol "><small class="font-weight-bold" >تعديل بياناتى</small></h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-4">
                 <a href="{{ route('account.orders') }}" class="position-relative saad border pt-3 pb-1 px-1  ">
 
                     <div class="">
@@ -25,7 +37,7 @@
                     </div>
                 </a>
                 </div>
-                <div class="col-6">
+                <div class="col-4">
 
                 <a href="{{ route('account.orders.create') }}" class="position-relative saad border pt-3 pb-1 px-1  ">
                     <div class="">
@@ -38,7 +50,7 @@
                     </div>
                 </a>
                 </div>
-                <div class="col-6">
+                <div class="col-4">
                 <a href="{{route('account.bills')}}" class="position-relative saad border pt-3 pb-1 px-1  ">
                     <div class="">
                     <div class="text-center">
@@ -50,17 +62,29 @@
                     </div>
                 </a>
                 </div>
-                <div class="col-6">
+                <div class="col-4 col-md-4">
                 <a href="" class="position-relative saad border pt-3 pb-1 px-1  ">
                     <div class="">
                     <div class="text-center">
-                        <i class="fas fa-coffee  icon-f"></i>
+                        <i class="fas fa-question-circle  icon-f"></i>
                     </div>
                     <div>
-                        <h5 class="text-center  manage  c-bol "><small class="font-weight-bold" >العروض الخاصة</small></h5>
+                        <h5 class="text-center  manage  c-bol "><small class="font-weight-bold" >أضف رأيك</small></h5>
                     </div>
                     </div>
                 </a>
+                </div>
+                <div class="col-4 col-md-4">
+                    <a href="" class="position-relative saad border pt-3 pb-1 px-1  ">
+                        <div class="">
+                            <div class="text-center">
+                                <i class="fas fa-sign-out-alt  icon-f"></i>
+                            </div>
+                            <div>
+                                <h5 class="text-center  manage  c-bol "><small class="font-weight-bold" >تسجيل الخروج</small></h5>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
 
