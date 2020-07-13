a08a351684f72ef6e0e713b98be8c646b3eefbdd
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2020-07-13 14:29:22 +0200

    0216515215

--- resources/views/admin/opinions/index.blade.php
+++ resources/views/admin/opinions/index.blade.php
@@ -2,25 +2,37 @@
 @section('content')
     <div class="container pt-3">
         <h3 class="col-12 text-center">رأي العملاء</h3>
-        @foreach(@App\Opinion::all() as $op)
-            <div class="card">
-                <div class="card-header">
-                    من
-                    {{$op->customer->user->name}}
-                    <form class="float-left" action="{{route('admin.opinions.destroy',$op)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" title="حذف" class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                        @if($op->statue == "pending")
-                        <a class="btn btn-success" title="الموافقة" href="{{route('admin.opinion_accept',$op)}}"><i class="fa fa-check"></i></a>
-                        <a class="btn btn-warning" title="الرفض" href="{{route('admin.opinion_refuse',$op)}}"><i class="fa fa-times"></i></a>
-                        @endif
-                    </form>
-                </div>
-                <div class="card-body">
-                    {{$op->body}}
-                </div>
-            </div>
-        @endforeach
+        <table class="table table-bordered text-center">
+            <tr>
+                <td>من</td>
+                <td>الرأي</td>
+                <td>الحالة</td>
+                <td>تحكم</td>
+            </tr>
+            @foreach(@App\Opinion::all() as $op)
+                <tr>
+                    <td>
+                        {{$op->customer->user->name}}
+                    </td>
+                    <td >
+                        {{$op->body}}
+                    </td>
+                    <td class="{{$op->statue == "pending"?"bg-info":($op->statue == "accepted"?"bg-success":"bg-danger")}}">
+                        {{$op->statue == "pending"?"بالانتظار":($op->statue == "accepted"?"مقبول":"مرفوض")}}
+                    </td>
+                    <td>
+                        <form class="float-left" action="{{route('admin.opinions.destroy',$op)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" title="حذف" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                            @if($op->statue == "pending")
+                                <a class="btn btn-success" title="الموافقة" href="{{route('admin.opinion_accept',$op)}}"><i class="fa fa-check"></i></a>
+                                <a class="btn btn-warning" title="الرفض" href="{{route('admin.opinion_refuse',$op)}}"><i class="fa fa-times"></i></a>
+                            @endif
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
